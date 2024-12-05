766a27e1f5f8afb2eb6568433e3baa8e02b09b73
RohanPrajapati1 <dp.kumarohanpr9528@gmail>
2024-12-05 22:34:49 +0530

    second page half completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,31 @@ module.exports = {
       'primary-text':'#747786',
       'secondary-text':'#3061F4',
       'bg-primary':'#ffffff',
-      'hover-primary':'#2950DA',
-      'hover-secondary':'#C6E5FB',
+      'bg-secondary':'#F3F7F8',
+      'hover-primary':'#0000EE',
+      'hover-secondary':'#EDF0FF',
       'from':'#EBF1FF',
-      'to':'#E5F3FE'
+      'to':'#E8F5FD'
     },
     extend: {
       spacing:{
         'heigth' : '87vh',
-        'right':'43px'
-      }
+        'right':'43px',
+        'weight':'93vw'
+      },
+      animation:{
+        move: "move 20s linear infinite",
+      },
+      keyframes:{
+        move:{
+          "0%":{
+            transform: "translateX(0)",
+        },
+          "100%":{
+            transform: "translateX(-100%)",
+          },
+        },
+      },
     },
   },
   plugins: [],
